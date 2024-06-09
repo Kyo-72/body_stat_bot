75c47e6f9672ba48e656e9dc87fb4e764594e5d8
Kyo-72 <seino0702@gmail.com>
2024-06-09 14:22:21 +0900

    chore : リポジトリをクラス定義するように変更

--- app/database/db_test.py
+++ app/database/db_test.py
@@ -1,6 +1,6 @@
 from database import engine, Base, get_session
 from datetime import datetime, timedelta
-from repository.health_data import bulk_insert_health_data, get_health_data_by_period
+from repository.health_data import HealthDataRepository
 from sqlalchemy.orm import Session
 from models.my_health_data import MyHealthData
 import pprint
@@ -17,18 +17,26 @@ data_list = [
     # 他のデータ...
 ]
 
-# セッションの取得
 with next(get_session()) as session:
-    new_records = bulk_insert_health_data(session, data_list)
-    for record in new_records:
-        print(record)
+    repository = HealthDataRepository(session)
+    
+    # # データの一括挿入
+    # data_list = [
+    #     {'weight': 70.5, 'bfp': 20.0, 'measurement_datetime': datetime(2023, 6, 10, 12, 0, 0)},
+    #     {'weight': 71.0, 'bfp': 19.8, 'measurement_datetime': datetime(2023, 6, 11, 12, 0, 0)},
+    #     # 他のデータ...
+    # ]
+    # new_records = repository.bulk_insert_health_data(data_list)
+    # for record in new_records:
+    #     print(record)
 
-# with next(get_session()) as session:
-#     start_date = datetime.now() - timedelta(days=7)
-#     end_date = datetime.now()
-#     health_data = get_health_data_by_period(session, start_date, end_date)
-#     for data in health_data:
-#         print(vars(data))
+    # 指定期間のデータ取得
+    start_date = datetime(2024, 6, 1)
+    end_date = datetime(2024, 6, 30)
+    health_data = repository.get_health_data_by_period(start_date, end_date)
+    for data in health_data:
+        print(vars(data))
+
 
 
 # データベースを作る処理
--- app/database/repository/health_data.py
+++ app/database/repository/health_data.py
@@ -2,40 +2,44 @@ import datetime
 from models.my_health_data import MyHealthData
 from sqlalchemy.orm import Session
 
-def bulk_insert_health_data(db: Session, data_list: list):
-    """
-    複数のMyHealthDataレコードを一度に挿入する関数
-    """
-    new_records = []
-    for data in data_list:
-        #重複するデータはDBに入れない
-        if( check_existing_measurement_datetime(db, data['measurement_datetime']) ):
-            continue
+class HealthDataRepository:
+    def __init__(self, db: Session):
+        self.db = db
 
-        new_records.append( MyHealthData(
-                weight=data['weight'],
-                bfp=data['bfp'],
-                measurement_datetime=data['measurement_datetime']
+    def bulk_insert_health_data(self, data_list: list):
+        """
+        複数のMyHealthDataレコードを一度に挿入する関数
+        """
+        new_records = []
+        for data in data_list:
+            # 重複するデータはDBに入れない
+            if self.__check_existing_measurement_datetime(data['measurement_datetime']):
+                continue
+
+            new_records.append(MyHealthData(
+                    weight=data['weight'],
+                    bfp=data['bfp'],
+                    measurement_datetime=data['measurement_datetime']
+                )
             )
-        )
-    
-    db.bulk_save_objects(new_records)
-    db.commit()
-    return new_records
+        
+        self.db.bulk_save_objects(new_records)
+        self.db.commit()
+        return new_records
 
-def check_existing_measurement_datetime(db: Session, measurement_datetime: datetime) -> bool:
-    """
-    同一のmeasurement_datetimeが存在するか確認する関数
-    """
-    return db.query(MyHealthData).filter(
-        MyHealthData.measurement_datetime == measurement_datetime
-    ).first() is not None
+    def __check_existing_measurement_datetime(self, measurement_datetime: datetime) -> bool:
+        """
+        同一のmeasurement_datetimeが存在するか確認する関数
+        """
+        return self.db.query(MyHealthData).filter(
+            MyHealthData.measurement_datetime == measurement_datetime
+        ).first() is not None
 
-def get_health_data_by_period(db: Session, start_date: datetime, end_date: datetime):
-    """
-    指定期間のMyHealthDataレコードを取得する関数
-    """
-    return db.query(MyHealthData).filter(
-        MyHealthData.measurement_datetime >= start_date,
-        MyHealthData.measurement_datetime <= end_date
-    ).all()
+    def get_health_data_by_period(self, start_date: datetime, end_date: datetime):
+        """
+        指定期間のMyHealthDataレコードを取得する関数
+        """
+        return self.db.query(MyHealthData).filter(
+            MyHealthData.measurement_datetime >= start_date,
+            MyHealthData.measurement_datetime <= end_date
+        ).all()
