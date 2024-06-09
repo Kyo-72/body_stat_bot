9783658349e304567fa2e55d358257a9d1d9ecff
Kyo-72 <seino0702@gmail.com>
2024-06-09 13:34:05 +0900

    Add : 指定期間取得関数追加/insertを重複なしに

--- app/database/crud.py
+++ app/database/crud.py
@@ -1,31 +1,41 @@
-from sqlalchemy.orm import Session
+import datetime
 from models.my_health_data import MyHealthData
-
-def insert_health_data(db: Session, weight: float, bfp: float, measurement_datetime):
-    """
-    単一のMyHealthDataレコードを挿入する関数
-    """
-    new_record = MyHealthData(
-        weight=weight,
-        bfp=bfp,
-        measurement_datetime=measurement_datetime
-    )
-    db.add(new_record)
-    db.commit()
-    db.refresh(new_record)
-    return new_record
+from sqlalchemy.orm import Session
 
 def bulk_insert_health_data(db: Session, data_list: list):
     """
     複数のMyHealthDataレコードを一度に挿入する関数
     """
-    new_records = [
-        MyHealthData(
-            weight=data['weight'],
-            bfp=data['bfp'],
-            measurement_datetime=data['measurement_datetime']
-        ) for data in data_list
-    ]
+    new_records = []
+    for data in data_list:
+        #重複するデータはDBに入れない
+        if( check_existing_measurement_datetime(db, data['measurement_datetime']) ):
+            continue
+
+        new_records.append( MyHealthData(
+                weight=data['weight'],
+                bfp=data['bfp'],
+                measurement_datetime=data['measurement_datetime']
+            )
+        )
+    
     db.bulk_save_objects(new_records)
     db.commit()
     return new_records
+
+def check_existing_measurement_datetime(db: Session, measurement_datetime: datetime) -> bool:
+    """
+    同一のmeasurement_datetimeが存在するか確認する関数
+    """
+    return db.query(MyHealthData).filter(
+        MyHealthData.measurement_datetime == measurement_datetime
+    ).first() is not None
+
+def get_health_data_by_period(db: Session, start_date: datetime, end_date: datetime):
+    """
+    指定期間のMyHealthDataレコードを取得する関数
+    """
+    return db.query(MyHealthData).filter(
+        MyHealthData.measurement_datetime >= start_date,
+        MyHealthData.measurement_datetime <= end_date
+    ).all()
--- app/database/db_test.py
+++ app/database/db_test.py
@@ -1,8 +1,9 @@
 from database import engine, Base, get_session
-from datetime import datetime
-from crud import insert_health_data, bulk_insert_health_data
+from datetime import datetime, timedelta
+from crud import bulk_insert_health_data, get_health_data_by_period
 from sqlalchemy.orm import Session
 from models.my_health_data import MyHealthData
+import pprint
 
 # セッションの取得
 # with next(get_session()) as session:
@@ -11,7 +12,7 @@ from models.my_health_data import MyHealthData
 
 # 挿入するデータのリスト
 data_list = [
-    {'weight': 70.5, 'bfp': 20.0, 'measurement_datetime': datetime.now()},
+    {'weight': 70.5, 'bfp': 20.0, 'measurement_datetime': datetime(2020, 8, 21)},
     {'weight': 71.0, 'bfp': 19.8, 'measurement_datetime': datetime.now()},
     # 他のデータ...
 ]
@@ -21,6 +22,13 @@ with next(get_session()) as session:
     new_records = bulk_insert_health_data(session, data_list)
     for record in new_records:
         print(record)
+
+# with next(get_session()) as session:
+#     start_date = datetime.now() - timedelta(days=7)
+#     end_date = datetime.now()
+#     health_data = get_health_data_by_period(session, start_date, end_date)
+#     for data in health_data:
+#         print(vars(data))
 
 
 # データベースを作る処理
