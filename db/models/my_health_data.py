0aef0ddabad3d816291b4d2d84cdb07b5fc23a24
Kyo-72 <seino0702@gmail.com>
2024-06-10 00:58:01 +0900

    chore : ディレクトリ構成変更/モジュールか

--- db/models/my_health_data.py
+++ db/models/my_health_data.py
@@ -5,6 +5,7 @@ from database import Base
 
 class MyHealthData(Base):
     __tablename__ = 'my_health_data'
+    __table_args__ = {'extend_existing': True} 
     id     = Column(Integer, primary_key=True, index=True)
     weight = Column(Numeric, nullable=False)
     bfp    = Column(Numeric, nullable=False)
