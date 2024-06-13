104af8fea377e07079f6b072d57c461e0de98423
Kyo-72 <seino0702@gmail.com>
2024-06-14 01:59:21 +0900

    add : DBから一週間の体重データを取得する

--- task/weekly_weight_summary.py
+++ task/weekly_weight_summary.py
@@ -1,5 +1,17 @@
+from db_layer.database import get_session
+from datetime import datetime, timedelta
+from repository.health_data_repository import HealthDataRepository
+
 def notify_weekly_average_weight():
+    #TODO これがここにあるの良くないかも
+    start_datetime = datetime.now() - timedelta(days = 7)
+    end_datetime     = datetime.now()
     #TODO DBからデータを取得
+    health_data_list = []
+    #TODO ここでrepository触るのは正しいの?かを検討する
+    with next(get_session()) as session:
+        repository = HealthDataRepository(session)
+        health_data_list = repository.get_health_data_by_period(start_datetime, end_datetime)
     #TODO データを整形
     #TODO 日別測定データをDBに入れるならここらでDBに保存しないといけないね
     #TODO 平均値を計算
