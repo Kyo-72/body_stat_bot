1169b61f69cd6f0ec955c7e384da10f22cdf44f6
Kyo-72 <seino0702@gmail.com>
2024-06-14 00:58:08 +0900

    Add : 平均体重/体脂肪率計算、通知メソッド 雛形作成(TODOコメント追加)

--- task/weekly_weight_summary.py
+++ task/weekly_weight_summary.py
@@ -1,2 +1,8 @@
 def notify_weekly_average_weight():
+    #TODO DBからデータを取得
+    #TODO データを整形
+    #TODO 日別測定データをDBに入れるならここらでDBに保存しないといけないね
+    #TODO 平均値を計算
+    #TODO メッセージを作成
+    #TODO slackで通知
     pass
